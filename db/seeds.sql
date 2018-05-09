20f6d3511b37921617aa2c598e1be6a96628cc4c
Chris Kim <chriskim@utexas.edu>
2018-05-08 22:53:51 -0500

    added logic and html/styling

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Double Cheese with Bacon", false),
+  ("Veggie Burger", true),
+  ("Chick-fil-a Sandwich", false),
+  ("Triple Meat Triple Cheese", false),
+  ("Big Mac", true),
+  ("Donut Burger", false),
+  ("Chili Cheese Burger", false);
